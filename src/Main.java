cd3293a175eb274b8a1196641a4ed66f427bba6c
Mikhail <mpolet@outlook.com>
2022-11-03 11:29:44 +0300

    Correct code

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-    CreditPaymentService service = new CreditPaymentService();
-    double amount = 1000000;
-    double months = 12;
-    double monthsTwo = 24;
-    double monthsThree = 36;
-    double payment = service.calculate(amount,months);
-    double paymentTwo = service.calculate(amount,monthsTwo);
-    double paymentThree = service.calculate(amount,monthsThree);
+        CreditPaymentService service = new CreditPaymentService();
+        double amount = 1000000;
+        double months = 12;
+        double monthsTwo = 24;
+        double monthsThree = 36;
+        double payment = service.calculate(amount, months);
+        double paymentTwo = service.calculate(amount, monthsTwo);
+        double paymentThree = service.calculate(amount, monthsThree);
         System.out.println("Ежемесячная плата:" + payment);
         System.out.println();
         System.out.println("Ежемесячная плата:" + paymentTwo);
